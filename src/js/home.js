dd8f984c78939442cf3ab8b6bf8b2e5b6d88722e
atiii <aty.sokhansanj@gmail.com>
2021-07-24 14:43:48 +0430

    first slider  added

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
     console.log('Hi')
+
+    $('#image-gallery').owlCarousel({
+        rtl: true,
+        nav: true,
+        items: 1,
+        loop: true,
+        dots: false,
+        center: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
+
+    $('.play').on('click', function () {
+        owl.trigger('play.owl.autoplay', [1000])
+    })
+    $('.stop').on('click', function () {
+        owl.trigger('stop.owl.autoplay')
+    })
 });
