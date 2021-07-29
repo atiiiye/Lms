c281268847871b998a79df00e05e2e19b7800fdf
atiii <aty.sokhansanj@gmail.com>
2021-07-29 14:44:28 +0430

    course page done

---
+++ src/js/course.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    console.log($('#courses-slider'))
+    $('#courses-slider').owlCarousel({
+        rtl: true,
+        loop: true,
+        margin: 10,
+        dots: false,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 3
+            },
+            991: {
+                items: 4
+            }
+        }
+    })
+})
