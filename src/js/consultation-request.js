b4c9fd0633ad2d04a97f64712f17b688a2f4fc8e
atiii <aty.sokhansanj@gmail.com>
2021-08-27 17:36:18 +0430

    consultation-request form fixed submit

--- src/js/consultation-request.js
+++ src/js/consultation-request.js
@@ -20,8 +20,53 @@ $(document).ready(function () {
     let submitButton = $('.contact-us-content__form__button')
     let form = $('.consultation-request__form')
 
-    submitButton.on('click', (event) => {
+    // submitButton.on('click', (event) => {
 
+    // if (NameField.val().length === 0) {
+    //     ErrorName.html("لطفا نام و نام خانوادگی خود را وارد کنید")
+    // } else if (!NameField.val().match(/^[آإأابپتثجچحخدذرزژسشصضطظعغفقکگلمنهۀةوؤیيئءًٌٍَُِّ\s]+$/)) {
+    //     ErrorName.html("نام و نام خانوادگی صحیح نمی باشد")
+    // } else {
+    //     ErrorName.html("")
+    // }
+
+    // if (PhoneNumberField.val().length === 0) {
+    //     ErrorPhonenumber.html("لطفا شماره تماس خود را وارد کنید")
+    // } else if (!PhoneNumberField.val().match(/^[0-9]+$/) || PhoneNumberField.val().length < 8) {
+    //     ErrorPhonenumber.html("شماره تماس صحیح نمی باشد")
+    // } else {
+    //     ErrorPhonenumber.html("")
+    // }
+
+    // if (!StatusField.is(":checked")) {
+    //     ErrorStatus.html("لطفا وضعیت مشاوره را تعیین کنید")
+    // } else {
+    //     ErrorStatus.html("")
+    // }
+
+    // if (!maritalStatusField.is(":checked")) {
+    //     ErrorMaritalStatus.html("لطفا وضعیت تاهل را تعیین کنید")
+    // } else {
+    //     ErrorMaritalStatus.html("")
+    // }
+
+    // if (!genderStatusField.is(":checked")) {
+    //     ErrorGenderStatus.html("لطفا جنسیت را تعیین کنید")
+    // } else {
+    //     ErrorGenderStatus.html("")
+    // }
+
+    // if (SubjectField.val().length === 0) {
+    //     ErrorSubject.html("لطفا موضوع مشاوره را وارد کنید")
+    // } else if (!SubjectField.val().match(/^[آإأابپتثجچحخدذرزژسشصضطظعغفقکگلمنهۀةوؤیيئءًٌٍَُِّ\s]+$/)) {
+    //     ErrorSubject.html("لطفا موضوع مشاوره را به درستی وارد کنید")
+    // } else {
+    //     ErrorSubject.html("")
+    // }
+    // })
+
+
+    NameField.on('input', () => {
         if (NameField.val().length === 0) {
             ErrorName.html("لطفا نام و نام خانوادگی خود را وارد کنید")
         } else if (!NameField.val().match(/^[آإأابپتثجچحخدذرزژسشصضطظعغفقکگلمنهۀةوؤیيئءًٌٍَُِّ\s]+$/)) {
@@ -29,7 +74,9 @@ $(document).ready(function () {
         } else {
             ErrorName.html("")
         }
+    })
 
+    PhoneNumberField.on('input', () => {
         if (PhoneNumberField.val().length === 0) {
             ErrorPhonenumber.html("لطفا شماره تماس خود را وارد کنید")
         } else if (!PhoneNumberField.val().match(/^[0-9]+$/) || PhoneNumberField.val().length < 8) {
@@ -37,25 +84,34 @@ $(document).ready(function () {
         } else {
             ErrorPhonenumber.html("")
         }
+    })
 
+    StatusField.on('input', () => {
         if (!StatusField.is(":checked")) {
             ErrorStatus.html("لطفا وضعیت مشاوره را تعیین کنید")
         } else {
             ErrorStatus.html("")
         }
+    })
 
+    maritalStatusField.on('input', () => {
         if (!maritalStatusField.is(":checked")) {
             ErrorMaritalStatus.html("لطفا وضعیت تاهل را تعیین کنید")
         } else {
             ErrorMaritalStatus.html("")
         }
+    })
+
+    genderStatusField.on('input', () => {
 
         if (!genderStatusField.is(":checked")) {
             ErrorGenderStatus.html("لطفا جنسیت را تعیین کنید")
         } else {
             ErrorGenderStatus.html("")
         }
+    })
 
+    SubjectField.on('input', () => {
         if (SubjectField.val().length === 0) {
             ErrorSubject.html("لطفا موضوع مشاوره را وارد کنید")
         } else if (!SubjectField.val().match(/^[آإأابپتثجچحخدذرزژسشصضطظعغفقکگلمنهۀةوؤیيئءًٌٍَُِّ\s]+$/)) {
@@ -65,7 +121,9 @@ $(document).ready(function () {
         }
     })
 
-    form.on('submit', (event) => {
+
+
+    form.submit(function (event) {
         if (NameField.val() &&
             PhoneNumberField.val() &&
             StatusField.is(":checked") &&
@@ -76,12 +134,56 @@ $(document).ready(function () {
             ErrorPhonenumber.html() == "" &&
             ErrorSubject.html() == ""
         ) {
-            form.submit()
+            $(this).submit(function () {
+                return true
+            })
             console.log('form called')
         }
         else {
             event.preventDefault()
             console.log('form not called!')
+
+            if (NameField.val().length === 0) {
+                ErrorName.html("لطفا نام و نام خانوادگی خود را وارد کنید")
+            } else if (!NameField.val().match(/^[آإأابپتثجچحخدذرزژسشصضطظعغفقکگلمنهۀةوؤیيئءًٌٍَُِّ\s]+$/)) {
+                ErrorName.html("نام و نام خانوادگی صحیح نمی باشد")
+            } else {
+                ErrorName.html("")
+            }
+
+            if (PhoneNumberField.val().length === 0) {
+                ErrorPhonenumber.html("لطفا شماره تماس خود را وارد کنید")
+            } else if (!PhoneNumberField.val().match(/^[0-9]+$/) || PhoneNumberField.val().length < 8) {
+                ErrorPhonenumber.html("شماره تماس صحیح نمی باشد")
+            } else {
+                ErrorPhonenumber.html("")
+            }
+
+            if (!StatusField.is(":checked")) {
+                ErrorStatus.html("لطفا وضعیت مشاوره را تعیین کنید")
+            } else {
+                ErrorStatus.html("")
+            }
+
+            if (!maritalStatusField.is(":checked")) {
+                ErrorMaritalStatus.html("لطفا وضعیت تاهل را تعیین کنید")
+            } else {
+                ErrorMaritalStatus.html("")
+            }
+
+            if (!genderStatusField.is(":checked")) {
+                ErrorGenderStatus.html("لطفا جنسیت را تعیین کنید")
+            } else {
+                ErrorGenderStatus.html("")
+            }
+
+            if (SubjectField.val().length === 0) {
+                ErrorSubject.html("لطفا موضوع مشاوره را وارد کنید")
+            } else if (!SubjectField.val().match(/^[آإأابپتثجچحخدذرزژسشصضطظعغفقکگلمنهۀةوؤیيئءًٌٍَُِّ\s]+$/)) {
+                ErrorSubject.html("لطفا موضوع مشاوره را به درستی وارد کنید")
+            } else {
+                ErrorSubject.html("")
+            }
         }
     })
 })
