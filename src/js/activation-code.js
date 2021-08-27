4676627aee990470ec0c49410468e2d39339195d
atiii <aty.sokhansanj@gmail.com>
2021-08-27 22:29:50 +0430

    activation-code form fixed submit

--- src/js/activation-code.js
+++ src/js/activation-code.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
-
-    console.log('code')
-
     let CodeField = $('.form__field#code')
 
     let ErrorCode = $('.form__field__error.code')
 
-    $('#submit').on('click', (event) => {
-        event.preventDefault()
 
+    CodeField.on('input', () => {
         if (CodeField.val().length === 0) {
             ErrorCode.html("لطفا کد فعالسازی را وارد کنید")
         } else if (!CodeField.val().match(/^[0-9]+$/)) {
@@ -18,5 +14,29 @@ $(document).ready(function () {
         } else {
             ErrorCode.html("")
         }
+    })
+
+    $('form').submit(function (event) {
+        if (CodeField.val() &&
+            ErrorCode.html() == ""
+        ) {
+            $(this).submit(function () {
+                return true
+            })
+        } else {
+            event.preventDefault()
+            if (CodeField.val().length === 0) {
+                ErrorCode.html("لطفا کد فعالسازی را وارد کنید")
+            } else if (!CodeField.val().match(/^[0-9]+$/)) {
+                ErrorCode.html("کد فعالسازی صحیح نمی باشد")
+            } else if (CodeField.val().match(/^[0-9]+$/) && CodeField.val().length !== 5) {
+                ErrorCode.html("کد فعالسازی باید پنج رقم باشد")
+            } else {
+                ErrorCode.html("")
+            }
+
+        }
+
+
     })
 })
