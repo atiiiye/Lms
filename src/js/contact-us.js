a88d14556fc18e0edbc078309b314682b78eb903
atiii <aty.sokhansanj@gmail.com>
2021-08-27 17:47:57 +0430

    contact-us form fixed submit

--- src/js/contact-us.js
+++ src/js/contact-us.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
 
     let NameField = $('.form__field#name')
@@ -10,9 +11,7 @@ $(document).ready(function () {
 
     let submitButton = $('.contact-us-content__form__button')
 
-    $('#submit').on('click', (event) => {
-        event.preventDefault()
-
+    NameField.on('input', () => {
         if (NameField.val().length === 0) {
             ErrorName.html("لطفا نام و نام خانوادگی خود را وارد کنید")
         } else if (!NameField.val().match(/^[آإأابپتثجچحخدذرزژسشصضطظعغفقکگلمنهۀةوؤیيئءًٌٍَُِّ\s]+$/)) {
@@ -20,8 +19,9 @@ $(document).ready(function () {
         } else {
             ErrorName.html("")
         }
+    })
 
-
+    PhoneNumberField.on('input', () => {
         if (PhoneNumberField.val().length === 0) {
             ErrorPhonenumber.html("لطفا شماره تماس خود را وارد کنید")
         } else if (!PhoneNumberField.val().match(/^[0-9]+$/) || PhoneNumberField.val().length < 8) {
@@ -29,17 +29,81 @@ $(document).ready(function () {
         } else {
             ErrorPhonenumber.html("")
         }
+    })
 
+    MessageField.on('input', () => {
         if (MessageField.val().length === 0) ErrorMessage.html("لطفا پیام خود را وارد کنید")
         else ErrorMessage.html("")
-
-        // if (NameField.val() &&
-        //     PhoneNumberField.val() &&
-        //     MessageField.val() &&
-        //     !ErrorName &&
-        //     !ErrorPhonenumber &&
-        //     !ErrorMessage) {
-        //     submitButton.prop("disabled", false)
-        // }
     })
+
+    $('form').submit(function (event) {
+        console.log($(this))
+
+        if (NameField.val() &&
+            PhoneNumberField.val() &&
+            MessageField.val() &&
+            ErrorName.html() == "" &&
+            ErrorPhonenumber.html() == "" &&
+            ErrorMessage.html() == ""
+        ) {
+            $(this).submit(function () {
+                return true
+            })
+        } else {
+            event.preventDefault()
+
+            if (NameField.val().length === 0) {
+                ErrorName.html("لطفا نام و نام خانوادگی خود را وارد کنید")
+            } else if (!NameField.val().match(/^[آإأابپتثجچحخدذرزژسشصضطظعغفقکگلمنهۀةوؤیيئءًٌٍَُِّ\s]+$/)) {
+                ErrorName.html("نام و نام خانوادگی صحیح نمی باشد")
+            } else {
+                ErrorName.html("")
+            }
+
+
+            if (PhoneNumberField.val().length === 0) {
+                ErrorPhonenumber.html("لطفا شماره تماس خود را وارد کنید")
+            } else if (!PhoneNumberField.val().match(/^[0-9]+$/) || PhoneNumberField.val().length < 8) {
+                ErrorPhonenumber.html("شماره تماس صحیح نمی باشد")
+            } else {
+                ErrorPhonenumber.html("")
+            }
+
+            if (MessageField.val().length === 0) ErrorMessage.html("لطفا پیام خود را وارد کنید")
+            else ErrorMessage.html("")
+        }
+    })
+
+    // $('#submit').on('click', (event) => {
+    // event.preventDefault()
+
+    // if (NameField.val().length === 0) {
+    //     ErrorName.html("لطفا نام و نام خانوادگی خود را وارد کنید")
+    // } else if (!NameField.val().match(/^[آإأابپتثجچحخدذرزژسشصضطظعغفقکگلمنهۀةوؤیيئءًٌٍَُِّ\s]+$/)) {
+    //     ErrorName.html("نام و نام خانوادگی صحیح نمی باشد")
+    // } else {
+    //     ErrorName.html("")
+    // }
+
+
+    // if (PhoneNumberField.val().length === 0) {
+    //     ErrorPhonenumber.html("لطفا شماره تماس خود را وارد کنید")
+    // } else if (!PhoneNumberField.val().match(/^[0-9]+$/) || PhoneNumberField.val().length < 8) {
+    //     ErrorPhonenumber.html("شماره تماس صحیح نمی باشد")
+    // } else {
+    //     ErrorPhonenumber.html("")
+    // }
+
+    // if (MessageField.val().length === 0) ErrorMessage.html("لطفا پیام خود را وارد کنید")
+    // else ErrorMessage.html("")
+
+    // if (NameField.val() &&
+    //     PhoneNumberField.val() &&
+    //     MessageField.val() &&
+    //     !ErrorName &&
+    //     !ErrorPhonenumber &&
+    //     !ErrorMessage) {
+    //     submitButton.prop("disabled", false)
+    // }
+    // })
 })
